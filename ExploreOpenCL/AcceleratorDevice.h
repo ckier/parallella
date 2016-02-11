2e3e170cd8d01727f3dbcdf557f3cd3f0f7d1b53
Christian K. Kier <chris.kier@devicecontrols.com>
2016-02-10 21:01:55 -0500

    parallella: #14: Create simple vector multiplication application for parallella * Add OpenCL context to AcceleratorDevice class.

--- ExploreOpenCL/AcceleratorDevice.h
+++ ExploreOpenCL/AcceleratorDevice.h
@@ -21,9 +21,12 @@ class AcceleratorDevice {
   log4cpp::Category* logger_;
   cl_platform_id platform_;
   cl_device_id device_;
+  cl_context context_;
   void InitializePlatform();
   void InitializeDevices();
-  void PrintPlatformInfo(cl_platform_id id, cl_platform_info info, std::string message);
-  void PrintDeviceInfo(cl_device_id id, cl_device_info info, std::string message);
+  void InitializeContexts();
+  void PrintPlatformInfo(cl_platform_info info, const std::string& message) const;
+  void PrintDeviceInfo(cl_device_info info, const std::string& message) const;
+  void PrintContextInfo(cl_context_info info, const std::string& message) const;
 };
 }  // namespace Parallella
